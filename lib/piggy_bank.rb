65bdec988d5aca12dd411b4890c2d91466850a78
Coral Baker <coral-ann@Corals-MacBook-Air.local>
2022-03-24 13:34:49 +0000

    Added/tested 'shake', with & without coin.

--- lib/piggy_bank.rb
+++ lib/piggy_bank.rb
@@ -1,4 +1,9 @@
 class PiggyBank
   def insert(coin)
+    @coin = coin
+  end
+
+  def shake
+    @coin.nil? ? "There's no sound." : "clink"
   end
 end
--- spec/piggy_bank_spec.rb
+++ spec/piggy_bank_spec.rb
@@ -3,7 +3,22 @@ require_relative '../lib/piggy_bank'
 describe PiggyBank do
   it 'Should accept coins' do
     piggy_bank = PiggyBank.new
-    coin = '50p'
+    coin = "50p"
     expect { piggy_bank.insert(coin) }.not_to raise_error
+  end
+
+  describe '#shake' do
+    it 'Should return "clink" when shaked' do
+      piggy_bank = PiggyBank.new
+      coin = "50p"
+      expect(piggy_bank.shake).to eq "There's no sound."
+    end
+
+    it 'Should return "clink" when shaked' do
+      piggy_bank = PiggyBank.new
+      coin = "50p"
+      piggy_bank.insert(coin)
+      expect(piggy_bank.shake).to eq "clink"
+    end
   end
 end
